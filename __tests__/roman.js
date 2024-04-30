67a52701d3071a8af5149dfe9d78d7b58857c4fe
Silas Dermer <dermers@oregonstate.edu>
2024-04-29 23:35:15 -0700

    TDD red: created test to loop until number is depleted

--- __tests__/roman.js
+++ __tests__/roman.js
@@ -11,3 +11,7 @@ test("Returns false if the input is not an integer (passes 1.2)", function () {
 test("Returns true since the input is an integer", function () {
     expect(roman(1000)).toBe("M")
 })
+
+test("tests that the while loop works", function () {
+    expect(roman(3000)).toBe("MMM")
+})
