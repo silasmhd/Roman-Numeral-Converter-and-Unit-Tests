f626743a5e80d360e89749bbee3968a318ac8b3d
Silas Dermer <dermers@oregonstate.edu>
2024-04-29 23:33:10 -0700

    TDD red: added a test for how to deal with and input of 1000

--- __tests__/roman.js
+++ __tests__/roman.js
@@ -4,3 +4,10 @@ test("Returns false if the input is not an integer (passes one)", function () {
     expect(roman("one")).toBe(false)
 })
 
+test("Returns false if the input is not an integer (passes 1.2)", function () {
+    expect(roman("1.2")).toBe(false)
+})
+
+test("Returns true since the input is an integer", function () {
+    expect(roman(1000)).toBe("M")
+})
