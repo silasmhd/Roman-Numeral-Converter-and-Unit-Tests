4c94a3f49e507adebba9c41d957e0cdc74bc54fe
Silas Dermer <dermers@oregonstate.edu>
2024-04-29 23:31:43 -0700

    TDD green: created an if statement to return false if not a number

--- __tests__/roman.js
+++ __tests__/roman.js
@@ -3,3 +3,4 @@ const roman = require("../roman")
 test("Returns false if the input is not an integer (passes one)", function () {
     expect(roman("one")).toBe(false)
 })
+
---
+++ roman.js
@@ -0,0 +1,12 @@
+module.exports = function roman(number) {
+    /*
+     * Check if number is not an integer
+     */
+    if (!Number.isInteger(number)) {
+        return false
+    }
+
+
+    return romanval;
+};
+
