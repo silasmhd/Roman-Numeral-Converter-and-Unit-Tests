c31ae4e91a1d949a25c97a0178ac8173b33dd8da
Silas Dermer <135365196+silasmhd@users.noreply.github.com>
2026-09-23 16:47:26 -0700

    Cleanup Capitolization

--- __tests__/roman.js
+++ __tests__/roman.js
@@ -12,23 +12,23 @@ test("Returns true since the input is an integer", function () {
     expect(roman(1000)).toBe("M")
 })
 
-test("tests that the while loop works", function () {
+test("Tests that the while loop works", function () {
     expect(roman(3000)).toBe("MMM")
 })
 
-test("returns true if each if statement runs once", function () {
+test("Returns true if each if statement runs once", function () {
     expect(roman(1166)).toBe("MCLXVI")
 })
 
-test("tests that 0 returns false", function () {
+test("Tests that 0 returns false", function () {
     expect(roman(0)).toBe(false)
 })
 
-test("tests that anythong above 3999 returns false", function () {
+test("Tests that anythong above 3999 returns false", function () {
     expect(roman(4000)).toBe(false)
 })
 
-test("tests that the while loop works", function () {
+test("Tests that the while loop works", function () {
     expect(roman(3999)).toBe("MMMCCCCCCCCCLXXXXVIIII")
 })
 
@@ -36,6 +36,6 @@ test("Returns false if the input is not an integer (passes 1.2)", function () {
     expect(roman(1.2)).toBe(false)
 })
 
-test("checks input of 1", function () {
+test("Checks input of 1", function () {
     expect(roman(1)).toBe("I")
 })
